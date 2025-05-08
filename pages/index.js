b7792f90cdf2610dec75a71e779059b6dcfd90bc
LunaraHQ <nathan@lunarahq.com>
2025-05-08 23:35:14 +0100

    Clean up homepage: removed FeatureGrid & PricingTable, streamlined layout

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,21 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
-import FeatureGrid from '../components/FeatureGrid'
-import HowItWorks from '../components/HowItWorks'
-import PricingTable from '../components/PricingTable'
+import HomeContent from '../components/HomeContent'
 import Footer from '../components/Footer'
-import ThemeToggle from '../components/ThemeToggle'
-
-// Defer heavy HomeContent (starfield + asteroid) to client
-const HomeContent = dynamic(() => import('../components/HomeContent'), {
-  ssr: false,
-})
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Lunara · Space-Age Funnels</title>
+        <title>Lunara – Launch Beyond</title>
+        <meta name="description" content="Lunara is your sales-focused SaaS funnel builder. Launch from the moon." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
-      <HomeContent />
+
       <HeroSection />
-      <FeatureGrid />
-      <HowItWorks />
-      <PricingTable />
+      <HomeContent />
       <Footer />
-      <ThemeToggle />
     </>
   )
 }
