ab25eca6f405658fb2989f99d6f17fb93f9f3d21
LunaraHQ <nathan@lunarahq.com>
2025-05-11 18:27:26 +0100

    Footer: Contact Us link opens ContactModal and matches other links

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
+// components/Footer.js
 import React from 'react'
 import Link from 'next/link'
-import ContactModal from './ContactModal'
+import ContactModal from './ContactModal'  // ← added
 
 export default function Footer() {
   return (
@@ -18,35 +19,47 @@ export default function Footer() {
 
         {/* Company links */}
         <div>
-          <h3 className="text-white font-semibold mb-2">Company</h3>
-          <ul className="space-y-1">
+          <h4 className="text-white font-semibold mb-2">Company</h4>
+          <ul className="space-y-2">
             <li>
               <Link href="/about">
-                <a>About Us</a>
+                <a className="hover:underline">About Us</a>
               </Link>
             </li>
             <li>
-              <ContactModal />
+              <ContactModal>
+                <a className="hover:underline">Contact Us</a>
+              </ContactModal>
             </li>
             <li>
               <Link href="/privacy">
-                <a>Privacy Policy</a>
+                <a className="hover:underline">Privacy Policy</a>
               </Link>
             </li>
           </ul>
         </div>
 
-        {/* Social links */}
+        {/* Social / Follow */}
         <div>
-          <h3 className="text-white font-semibold mb-2">Follow</h3>
-          <ul className="space-y-1">
+          <h4 className="text-white font-semibold mb-2">Follow</h4>
+          <ul className="space-y-2">
             <li>
-              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://twitter.com/yourhandle"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
                 Twitter
               </a>
             </li>
             <li>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://linkedin.com/yourcompany"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
                 LinkedIn
               </a>
             </li>
