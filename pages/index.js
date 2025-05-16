10eaab5b8b0dcac2fcab7c9bf10589a68c838765
LunaraHQ <nathan@lunarahq.com>
2025-05-16 23:26:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import HeroSection from "../components/HeroSection";
-import FeatureGrid from "../components/FeatureGrid";
 import HomeContent from "../components/HomeContent";
+import HowItWorks from "../components/HowItWorks";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <main className="bg-gradient-to-br from-[#1a103e] via-[#6E41FF] to-[#130b24] min-h-screen flex flex-col">
-      {/* Main Hero Section */}
       <HeroSection />
-      {/* Feature Grid Section (3 highlights) */}
-      <FeatureGrid />
-      {/* Modular Features + Security + Pricing */}
       <HomeContent />
-      {/* Footer */}
+      <HowItWorks />
       <Footer />
     </main>
   );
