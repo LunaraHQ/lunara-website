e46a6ddae55670c307d084f8ba85545754a9abf1
LunaraHQ <nathan@lunarahq.com>
2025-05-17 09:04:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import HeroSection from "../components/HeroSection";
 import HomeContent from "../components/HomeContent";
 import HowItWorks from "../components/HowItWorks";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 // import FeatureCards from "../components/features/FeatureCards"; // If needed
 
 export default function Home() {
@@ -25,11 +26,27 @@ export default function Home() {
   return (
     <main className="bg-gradient-to-br from-[#140a29] via-[#341a66] to-[#6E41FF] min-h-screen">
       <HeroSection />
+
       <HomeContent />
+
+      {/* SVG Separator */}
+      <div className="w-full">
+        <svg width="100%" height="60" viewBox="0 0 1920 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 60C400 20 1520 100 1920 60V0H0V60Z" fill="#1a1336" fillOpacity="0.16"/>
+        </svg>
+      </div>
+
       <HowItWorks />
-      {/* <FeatureCards /> */}
-      {/* <PricingTable /> REMOVE THIS LINE */}
+
+      {/* Another SVG Separator before Footer (optional) */}
+      <div className="w-full">
+        <svg width="100%" height="60" viewBox="0 0 1920 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0C400 40 1520 -40 1920 0V60H0V0Z" fill="#1a1336" fillOpacity="0.18"/>
+        </svg>
+      </div>
+
       <Footer />
+      <ScrollToTop />
     </main>
   );
 }
